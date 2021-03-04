514127d9555a6092998157bbf5ffce9244c56a77
Chris Lloyd <lloydcd@clarkson.edu>
2021-03-04 17:14:12 -0500

    Added initial user logic for I2C ADC Driver (untested)

--- driver_modules/ad_da_development/ad_dev/adc_i2c_driver.vhd
+++ driver_modules/ad_da_development/ad_dev/adc_i2c_driver.vhd
@@ -1,11 +1,13 @@
 --------------------------------------------------------------------------------
--- Filename     : dac_i2c_driver.vhd
+-- Filename     : adc_i2c_driver.vhd
 -- Author(s)    : Chris Lloyd, Thomas Griebel, Anthony Seybolt
 -- Class        : EE316 (Project 3)
 -- Due Date     : 2021-02-23
 -- Target Board : Cora 7Z010
--- Entity       : dac_i2c_driver
--- Description  : CDL=> Uses:
+-- Entity       : adc_i2c_driver
+-- Description  : Module to get analog data from a PCF8591T I2C A/D and D/A
+--                converter.
+--                CDL=> Add more here
 --                https://www.digikey.com/eewiki/pages/viewpage.action?pageId=10125324
 --------------------------------------------------------------------------------
 
@@ -13,13 +15,13 @@
 --  Libraries  --
 -----------------
 library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 --------------
 --  Entity  --
 --------------
-entity dac_i2c_driver is
+entity adc_i2c_driver is
 generic
 (
   C_CLK_FREQ_MHZ : integer := 125                     -- System clock frequency in MHz
@@ -29,27 +31,29 @@ port
   I_CLK          : in std_logic;                      -- System clk frequency of (C_CLK_FREQ_MHZ)
   I_RESET_N      : in std_logic;                      -- System reset (active low)
 
-  I_DAC_ENABLE   : in std_logic;                      -- Whether the screen is on '1' or off '0'
+  I_ADC_ENABLE   : in std_logic;                      -- Whether the adc is enabled
 
-  I_DAC_DATA     : in std_logic_vector(7 downto 0);   -- Data to generate Digital to analog output
-  O_BUSY         : out std_logic;                     -- Busy signal from I2C master
+  I_ADC_CH_NUM   : in std_logic(1 downto 0);          -- CDL=> Here
+
+  O_DAC_DATA     : out std_logic_vector(7 downto 0);  -- Data from A/D conversion
 
   IO_I2C_SDA     : inout std_logic;                   -- Serial data of i2c bus
   IO_I2C_SCL     : inout std_logic                    -- Serial clock of i2c bus
 );
-end entity dac_i2c_driver;
+end entity adc_i2c_driver;
 
 --------------------------------
 --  Architecture Declaration  --
 --------------------------------
-architecture behavioral of dac_i2c_driver is
+architecture behavioral of adc_i2c_driver is
+
   ----------------
   -- Components --
   ----------------
   component i2c_master is
   generic
   (
-    input_clk : integer := 125_000_000;               -- Input clock speed from user logic in Hz
+    input_clk : integer := 125_000_000;              -- Input clock speed from user logic in Hz
     bus_clk   : integer := 100_000                   -- Speed the i2c bus (scl) will run at in Hz
   );
   port
@@ -67,37 +71,36 @@ architecture behavioral of dac_i2c_driver is
     scl       : inout  std_logic                     -- Serial clock output of i2c bus
   );
   end component i2c_master;
+
   ---------------
   -- Constants --
   ---------------
+
   constant C_CLK_FREQ_HZ         : integer := C_CLK_FREQ_MHZ * 1_000_000;
   constant C_I2C_BUS_CLK_FREQ_HZ : integer := 100_000;
 
-  constant C_I2C_DAC_ADDR        : std_logic_vector(7 downto 0) := x"48";  -- Default address is 0x48
-  constant C_WR_BYTE_INDEX_MAX   : integer := 1; -- CDL=> Here
-
-  constant C_I2C_DAC_CTRL_BYTE   : std_logic_vector(7 downto 0) := x"40";
+  constant C_I2C_ADC_ADDR        : std_logic_vector(6 downto 0) := x"24";  -- CDL=> May need to be "0x48"
 
   -------------
   -- SIGNALS --
   -------------
 
   -- State machine related signals
-  type T_DAC_STATE is (READY_STATE, WRITE_STATE, WAIT_STATE, NEXT_STATE);
-  signal s_i2c_curr_state       : T_DAC_STATE := READY_STATE;
+  type T_DAC_STATE is (WRITE_CONTROL_STATE, READ_ANALOG_STATE, WAIT_STATE);
+  signal s_i2c_curr_state       : T_DAC_STATE := WAIT_STATE;
 
   signal s_i2c_enable           : std_logic;
+  signal s_i2c_address          : std_logic_vector(6 downto 0) := C_I2C_ADC_ADDR;
+  signal s_i2c_rw               : std_logic;  --'0' is write, '1' is read
   signal s_wr_data_byte         : std_logic_vector(7 downto 0);
-  signal s_wr_data_byte_index   : integer;
-
-  signal s_i2c_wr               : std_logic;  --'0' is write, '1' is read
-  signal s_i2c_address          : std_logic_vector(6 downto 0) := C_I2C_DAC_ADDR(6 downto 0);
+  signal s_rw_data_byte         : std_logic_vector(7 downto 0);
   signal s_i2c_busy             : std_logic;
-  signal s_data_latched         : std_logic_vector(7 downto 0);
+  signal s_i2c_busy_prev        : std_logic;
+  signal s_adc_ch_num_prev      : std_logic_vector(1 downto 0);
 
 begin
 
-  -- I2C controller for 4 digit Seven Segment Display
+  -- I2C controller for ADC
   I2C_MASTER_INST:i2c_master
   generic map
   (
@@ -110,10 +113,10 @@ begin
     reset_n   => I_RESET_N,
     ena       => s_i2c_enable,
     addr      => s_i2c_address,
-    rw        => s_i2c_wr,
+    rw        => s_i2c_rw,
     data_wr   => s_wr_data_byte,
     busy      => s_i2c_busy,
-    data_rd   => open,
+    data_rd   => s_rw_data_byte,
     ack_error => open,
     sda       => IO_I2C_SDA,
     scl       => IO_I2C_SCL
@@ -127,152 +130,95 @@ begin
   -- Process Name     : I2C_STATE_MACHINE
   -- Sensitivity List : I_CLK            : System clock
   --                    I_RESET_N        : System reset (active low logic)
-  -- Useful Outputs   : s_i2c_curr_state : Current state of S7D write process.
-  -- Description      : State machine to control different states for I2C
-  --                    Seven Segment Display (7SD) module.
+  -- Useful Outputs   : s_i2c_curr_state : The current state of the system
+  -- Description      : State machine process to control the ADC I2C master.
   ------------------------------------------------------------------------------
   I2C_STATE_MACHINE: process (I_CLK, I_RESET_N)
   begin
     if (I_RESET_N = '0') then
-      s_i2c_curr_state             <= READY_STATE;
+      s_i2c_curr_state         <= WAIT_STATE;
 
     elsif (rising_edge(I_CLK)) then
+      if (I_ADC_ENABLE = '0') then
+        s_i2c_curr_state       <= WAIT_STATE
+      else
         -- I2C state machine logic
         case s_i2c_curr_state is
-          when READY_STATE =>
-            if (s_data_latched /= I_DAC_DATA) then
-              s_i2c_curr_state     <= WRITE_STATE;
-            else
-              s_i2c_curr_state     <= s_i2c_curr_state;
-            end if;
-
-          when WRITE_STATE =>
-            s_i2c_curr_state       <= WAIT_STATE;
-
+          -- Transition state to wait for I2C to be no longer busy
           when WAIT_STATE =>
-            if (s_i2c_busy = '1') then
-              s_i2c_curr_state     <= NEXT_STATE;
+            if (s_i2c_busy = '0') then
+              s_i2c_curr_state <= WRITE_CONTROL_STATE
             else
-              s_i2c_curr_state     <= s_i2c_curr_state;
+              s_i2c_curr_state <= WAIT_STATE
             end if;
 
-            when NEXT_STATE =>
-              if (s_i2c_busy = '0') then
-                if (s_wr_data_byte_index /= C_WR_BYTE_INDEX_MAX) then
-                  s_i2c_curr_state <= WRITE_STATE;
-                else
-                  s_i2c_curr_state <= READY_STATE;
-                end if;
-              else
-                s_i2c_curr_state   <= s_i2c_curr_state;
-              end if;
+          -- Write the control byte indicating what channel to use
+          when WRITE_CONTROL_STATE =>
+          if ((s_i2c_busy_prev = '0') and (s_i2c_busy = '1')) then
+            s_i2c_curr_state   <= READ_ANALOG_STATE
+          else
+            s_i2c_curr_state   <= WRITE_CONTROL_STATE
+          end if;
+
+          -- Contiously read values from the ADC until a new channel is selected
+          when READ_ANALOG_STATE =>
+            if (s_adc_ch_num_prev /= I_ADC_CH_NUM) then
+              s_i2c_curr_state <= WAIT_STATE;
+            else
+              s_i2c_curr_state <= READ_ANALOG_STATE;
+            end if;
 
           -- Error condition, should never occur
           when others =>
-            s_i2c_curr_state       <= READY_STATE;
+            s_i2c_curr_state   <= WAIT_STATE;
         end case;
+      end if;
     end if;
   end process I2C_STATE_MACHINE;
   ------------------------------------------------------------------------------
 
   ------------------------------------------------------------------------------
   -- Process Name     : DATA_FLOW_CTRL
-  -- Sensitivity List : I_CLK                  : System clock
-  --                    I_RESET_N              : System reset (active low logic)
-  -- Useful Outputs   : s_display_data_latched : Latched data to remain stable.
-  --                    s_7sd_enable           : Enable signal for I2C master.
-  --                    s_wr_data_byte_index   : Current data byte index.
-  --                    O_BUSY                 : Output busy signal.
-  -- Description      : Process to increment index counter representing current
-  --                    data byte to send. LUT table in (WRITE_DATA_LUT). Also
-  --                    controls enable signal for I2C master, and output busy
-  --                    signal.
+  -- Sensitivity List : I_CLK             : System clock
+  --                    I_RESET_N         : System reset (active low logic)
+  -- Useful Outputs   : s_i2c_busy_prev   : Previous busy signal value.
+  --                    s_adc_ch_num_prev : Previous channel num value.
+  --                    s_i2c_enable      : Enable signal for I2C master.
+  --                    s_i2c_rw          : Read/write for I2C master.
+  -- Description      : A process to control the flow of data from and to the
+  --                    I2C A/D device.
   ------------------------------------------------------------------------------
   DATA_FLOW_CTRL: process (I_CLK, I_RESET_N)
   begin
     if (I_RESET_N = '0') then
-      s_data_latched     <= (others=>'0');
-      s_i2c_enable               <= '0';
-      s_wr_data_byte_index       <=  0;
-      O_BUSY                     <= '1';
+      s_i2c_busy_prev   <= '1';
+      s_adc_ch_num_prev <= "00";
+      s_i2c_enable      <= '0';
+      s_i2c_rw          <= '1';
 
     elsif (rising_edge(I_CLK)) then
-      if (I_DAC_ENABLE = '1') then
-      
-      if ()
-      
-        -- Latch data so it does not change during write
-        if (s_i2c_curr_state = READY_STATE) then
-          s_data_latched  <= I_DAC_DATA;
-        else
-          s_data_latched  <= s_data_latched;
-        end if;
+      -- Latch data inputs for edge detection
+      s_i2c_busy_prev   <= s_i2c_busy;
+      s_adc_ch_num_prev <= I_ADC_CH_NUM;
 
-        -- Enable signal logic
---        if (s_i2c_curr_state = WRITE_STATE) then
---          s_i2c_enable             <= '1';
---        elsif (s_i2c_curr_state = WAIT_STATE) and
---              (s_i2c_busy = '1') then
---          s_i2c_enable             <= '0';
---        else
---          s_i2c_enable             <= s_i2c_enable;
---        end if;
-        if (s_i2c_curr_state /= READY_STATE) then
-          s_i2c_enable             <= '1';
-        else
-          s_i2c_enable             <= '0';
-        end if;
-
-        -- Data Byte Index logic
-        if (s_i2c_curr_state = NEXT_STATE) and (s_i2c_busy = '0') then
-          if (s_wr_data_byte_index /= C_WR_BYTE_INDEX_MAX) then
-            s_wr_data_byte_index <= s_wr_data_byte_index + 1;
-          else
-            s_wr_data_byte_index <= 0;
-          end if;
-        else
-          s_wr_data_byte_index     <= s_wr_data_byte_index;
-        end if;
-
-        -- Output Busy logic
-        if (s_i2c_curr_state = READY_STATE) then
-          O_BUSY                   <= '0';
-        else
-          O_BUSY                   <= '1';
-        end if;
+      -- Enable signal logic
+      if (s_i2c_curr_state /= WAIT_STATE) then
+        s_i2c_enable    <= '1';
       else
-        s_data_latched     <= (others=>'1');
-        s_i2c_enable               <= '0';
-        s_wr_data_byte_index       <=  0;
-        O_BUSY                     <= '1';      
+        s_7sd_enable    <= '0';
+      end if;
+
+      -- Read / Write logic
+      if (s_i2c_curr_state = WRITE_CONTROL_STATE) then
+        s_i2c_rw        <= '0'; -- Writing
+      else
+        s_i2c_rw        <= '1'; -- Reading
       end if;
     end if;
   end process DATA_FLOW_CTRL;
   ------------------------------------------------------------------------------
 
-  ------------------------------------------------------------------------------
-  -- Process Name     : WRITE_DATA_LUT
-  -- Sensitivity List : I_CLK          : System clock
-  --                    I_RESET_N      : System reset (active low logic)
-  -- Useful Outputs   : s_wr_data_byte : Data byte to send to Display
-  -- Description      : Process to act as Look Up Table (LUT) to decide what
-  --                    byte to send to display based on (s_wr_data_byte_index).
-  ------------------------------------------------------------------------------
-  WRITE_DATA_LUT: process (I_CLK, I_RESET_N)
-  begin
-    if (I_RESET_N = '0') then
-      s_wr_data_byte                  <= C_I2C_DAC_CTRL_BYTE;
-    elsif (rising_edge(I_CLK)) then
-      case s_wr_data_byte_index is
---        when 0      => s_wr_data_byte <= C_I2C_DAC_ADDR;
-        when 0      => s_wr_data_byte <= C_I2C_DAC_CTRL_BYTE;
-        when 1      => s_wr_data_byte <= s_data_latched;
-        when others => s_wr_data_byte <= s_data_latched;
-      end case;
-    end if;
-  end process WRITE_DATA_LUT;
-  ------------------------------------------------------------------------------
-
-  s_i2c_wr <= '0'; -- Always writing
+  s_wr_data_byte <= "000000" & I_ADC_CH_NUM;  -- Write data to I2C master (always control byte)
+  O_DAC_DATA     <= s_rw_data_byte;           -- Read data (always output ADC data)
 
 end architecture behavioral;
