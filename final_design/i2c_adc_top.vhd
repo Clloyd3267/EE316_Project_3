f8fbafd040e8b0486cbac1d200e3d5e77503f21a
Chris Lloyd <lloydcd@clarkson.edu>
2021-03-08 17:22:48 -0500

    V2 of final design. Added debounce and edge trigger modules for btn inputs.

--- final_design/i2c_adc_top.vhd
+++ final_design/i2c_adc_top.vhd
@@ -5,7 +5,7 @@
 -- Due Date     : 2021-03-11
 -- Target Board : Cora 7Z010
 -- Entity       : i2c_adc_top
--- Description  : CDL=> Top level entity add more here
+-- Description  : CDL=> Top level entity add description here
 --------------------------------------------------------------------------------
 
 -----------------
@@ -14,6 +14,10 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
+
+-- For edge triggering enum type
+library work;
+  use work.edge_detector_utilities.all;
 
 --------------
 --  Entity  --
@@ -98,55 +102,88 @@ architecture behavioral of i2c_adc_top is
   end component pwm_driver;
 
   --CDL=> Add LCD components
-  --CDL=> Add DAC components
+  --CDL=> Add DAC components (may be included in ADC module)
+
+  component debounce_button is
+  generic
+  (
+    C_CLK_FREQ_MHZ   : integer := 50;  -- System clock frequency in MHz
+    C_STABLE_TIME_MS : integer := 10   -- Time required for button to remain stable in ms
+  );
+  port
+  (
+    I_CLK            : in std_logic;  -- System clk frequency of (C_CLK_FREQ_MHZ)
+    I_RESET_N        : in std_logic;  -- System reset (active low)
+    I_BUTTON         : in std_logic;  -- Button data to be debounced
+    O_BUTTON         : out std_logic  -- Debounced button data
+  );
+  end component debounce_button;
+
+  component edge_detector is
+  generic
+  (
+    C_CLK_FREQ_MHZ   : integer     := 50;   -- System clock frequency in MHz
+    C_TRIGGER_EDGE   : T_EDGE_TYPE := NONE  -- Edge to trigger on
+  );
+  port
+  (
+    I_CLK            : in std_logic;  -- System clk frequency of (C_CLK_FREQ_MHZ)
+    I_RESET_N        : in std_logic;  -- System reset (active low)
+    I_SIGNAL         : in std_logic;  -- Input signal to pass through edge detector
+    O_EDGE_SIGNAL    : out std_logic  -- Output pulse on (C_TRIGGER_EDGE) edge
+  );
+  end component edge_detector;
 
   ---------------
   -- Constants --
   ---------------
 
-  constant C_CLK_FREQ_MHZ   : integer := 125;  -- System clock frequency in MHz
+  constant C_CLK_FREQ_MHZ  : integer := 125;  -- System clock frequency in MHz
 
-  -- ADC channel number to get data from CDL=> List devices here
-  constant C_ADC_CH_0       : std_logic_vector(1 downto 0) := "00";
-  constant C_ADC_CH_1       : std_logic_vector(1 downto 0) := "01";
-  constant C_ADC_CH_2       : std_logic_vector(1 downto 0) := "10";
-  constant C_ADC_CH_3       : std_logic_vector(1 downto 0) := "11";
+  -- ADC channel number / Data inputs
+  constant C_ADC_CH_0      : std_logic_vector(1 downto 0) := "00";  -- LDR
+  constant C_ADC_CH_1      : std_logic_vector(1 downto 0) := "01";  -- TEMP
+  constant C_ADC_CH_2      : std_logic_vector(1 downto 0) := "10";  -- POT
+  constant C_ADC_CH_3      : std_logic_vector(1 downto 0) := "11";  -- ANALOG IN
 
-  -- Data outputs -- CDL=> List later
-  constant C_OUT_PWM        : std_logic_vector(1 downto 0) := "00";
-  constant C_OUT_CLK        : std_logic_vector(1 downto 0) := "01";
-  constant C_OUT_DAC        : std_logic_vector(1 downto 0) := "10";
-  constant C_OUT_DISABLED   : std_logic_vector(1 downto 0) := "11";  -- CDL=> Remove later
+  -- Data outputs
+  constant C_OUT_PWM       : std_logic_vector(1 downto 0) := "00";
+  constant C_OUT_CLK       : std_logic_vector(1 downto 0) := "01";
+  constant C_OUT_DAC       : std_logic_vector(1 downto 0) := "10";
+  constant C_OUT_DISABLED  : std_logic_vector(1 downto 0) := "11";
 
-  constant C_LED_BLUE     : std_logic_vector(2 downto 0) := "001";
-  constant C_LED_GREEN    : std_logic_vector(2 downto 0) := "010";
-  constant C_LED_RED      : std_logic_vector(2 downto 0) := "100";
-  constant C_LED_OFF      : std_logic_vector(2 downto 0) := "000";
+  constant C_LED_BLUE      : std_logic_vector(2 downto 0) := "001";
+  constant C_LED_GREEN     : std_logic_vector(2 downto 0) := "010";
+  constant C_LED_RED       : std_logic_vector(2 downto 0) := "100";
+  constant C_LED_OFF       : std_logic_vector(2 downto 0) := "000";
 
   -- Inital System reset time in ms
-  constant C_RESET_TIME_MS  : integer := 25;
+  constant C_RESET_TIME_MS : integer := 25;
 
   -------------
   -- SIGNALS --
   -------------
 
-  signal s_reset_n          : std_logic;                     -- Active low system reset
+  signal s_reset_n         : std_logic;                     -- Active low system reset
 
-  signal s_adc_enable       : std_logic;                     -- Enable signal for ADC module
-  signal s_adc_ch_num       : std_logic_vector(1 downto 0);  -- Channel number for ADC module
-  signal s_adc_data         : std_logic_vector(7 downto 0);  -- Data from ADC
+  signal s_adc_enable      : std_logic;                     -- Enable signal for ADC module
+  signal s_adc_ch_num      : std_logic_vector(1 downto 0);  -- Channel number for ADC module
+  signal s_adc_data        : std_logic_vector(7 downto 0);  -- Data from ADC
 
-  signal s_clk_enable       : std_logic;                     -- Enable signal for CLK gen module
-  signal s_pwm_enable       : std_logic;                     -- Enable signal for PWM gen module
-  signal s_dac_enable       : std_logic;                     -- Enable signal for DAC gen module
+  signal s_clk_enable      : std_logic;                     -- Enable signal for CLK gen module
+  signal s_pwm_enable      : std_logic;                     -- Enable signal for PWM gen module
+  signal s_dac_enable      : std_logic;                     -- Enable signal for DAC gen module
 
-  signal s_output_mode      : std_logic_vector(1 downto 0);  -- Data output mode
+  signal s_output_mode     : std_logic_vector(1 downto 0);  -- Data output mode
 
-  signal s_led_0            : std_logic_vector(2 downto 0);  -- LED0
-  signal s_led_1            : std_logic_vector(2 downto 0);  -- LED1
+  signal s_led_0           : std_logic_vector(2 downto 0);  -- LED0 output
+  signal s_led_1           : std_logic_vector(2 downto 0);  -- LED1 output
 
-  signal s_btn_0_prev       : std_logic;
-  signal s_btn_1_prev       : std_logic;
+  signal s_debounced_btn_0 : std_logic;                     -- Debounced BTN0 signal
+  signal s_debounced_btn_1 : std_logic;                     -- Debounced BTN1 signal
+
+  signal s_edge_trig_btn_0 : std_logic;                     -- Edge triggered BTN0 signal
+  signal s_edge_trig_btn_1 : std_logic;                     -- Edge triggered BTN1 signal
 
 begin
   ------------------------------
@@ -198,7 +235,65 @@ begin
   );
 
   --CDL=> Add LCD component instantions
-  --CDL=> Add DAC component instantions
+  --CDL=> Add DAC component instantions (may be included in ADC module)
+
+  -- Device driver for BTN0 debounce module
+  DEBOUNCE_BTN0_INST: debounce_button
+  generic map
+  (
+    C_CLK_FREQ_MHZ => C_CLK_FREQ_MHZ
+  )
+  port map
+  (
+    I_CLK          => I_CLK_125_MHZ,
+    I_RESET_N      => s_reset_n,
+    I_BUTTON       => I_BTN_0,
+    O_BUTTON       => s_debounced_btn_0
+  )
+
+  -- Device driver for BTN1 debounce module
+  DEBOUNCE_BTN1_INST: debounce_button
+  generic map
+  (
+    C_CLK_FREQ_MHZ => C_CLK_FREQ_MHZ
+  )
+  port map
+  (
+    I_CLK          => I_CLK_125_MHZ,
+    I_RESET_N      => s_reset_n,
+    I_BUTTON       => I_BTN_1,
+    O_BUTTON       => s_debounced_btn_1
+  )
+
+  -- Device driver for BTN0 edge trigger module
+  EDGE_TRIG_BTN0_INST: edge_detector
+  generic map
+  (
+    C_CLK_FREQ_MHZ => C_CLK_FREQ_MHZ,
+    C_TRIGGER_EDGE => RISING
+  )
+  port map
+  (
+    I_CLK          => I_CLK_125_MHZ,
+    I_RESET_N      => s_reset_n,
+    I_SIGNAL       => s_debounced_btn_0,
+    O_EDGE_SIGNAL  => s_edge_trig_btn_0
+  )
+
+  -- Device driver for BTN1 edge trigger module
+  EDGE_TRIG_BTN1_INST: edge_detector
+  generic map
+  (
+    C_CLK_FREQ_MHZ => C_CLK_FREQ_MHZ,
+    C_TRIGGER_EDGE => RISING
+  )
+  port map
+  (
+    I_CLK          => I_CLK_125_MHZ,
+    I_RESET_N      => s_reset_n,
+    I_SIGNAL       => s_debounced_btn_1,
+    O_EDGE_SIGNAL  => s_edge_trig_btn_1
+  )
 
   ---------------
   -- Processes --
@@ -217,11 +312,11 @@ begin
   begin
     if (rising_edge(I_CLK_125_MHZ)) then
       if (v_reset_cntr = C_RST_MS_DURATION) then
-        v_reset_cntr    := v_reset_cntr;
-        s_reset_n <= '1';
+        v_reset_cntr := v_reset_cntr;
+        s_reset_n    <= '1';
       else
-        v_reset_cntr    := v_reset_cntr + 1;
-        s_reset_n <= '0';
+        v_reset_cntr := v_reset_cntr + 1;
+        s_reset_n    <= '0';
       end if;
     end if;
   end process SYSTEM_RST_OUTPUT;
@@ -239,18 +334,12 @@ begin
   INOUT_MODE_CONTROL: process (I_CLK_125_MHZ)
   begin
     if (s_reset_n = '0') then
-      s_adc_ch_num        <= C_ADC_CH_3;  -- CDL=> Change default values
-      s_output_mode       <= C_OUT_CLK;   -- CDL=> Change default values
-      s_btn_0_prev        <= '0';
-      s_btn_1_prev        <= '0';
+      s_adc_ch_num        <= C_ADC_CH_3;  -- Default values for input mode
+      s_output_mode       <= C_OUT_CLK;   -- Default values for output mode
 
     elsif (rising_edge(I_CLK_125_MHZ)) then
 
-      -- Latch the button inputs
-      s_btn_0_prev        <= I_BTN_0;
-      s_btn_1_prev        <= I_BTN_1;
-
-      if (s_btn_0_prev = '0' and I_BTN_0 = '1') then
+      if (s_edge_trig_btn_1 = '1') then
         -- Toggle input mode
         case (s_adc_ch_num) is
           when C_ADC_CH_0 =>
@@ -268,7 +357,7 @@ begin
         s_adc_ch_num      <= s_adc_ch_num;
       end if;
 
-      if (s_btn_1_prev = '0' and I_BTN_1 = '1') then
+      if (s_edge_trig_btn_0 = '1') then
         -- Toggle output mode
         case (s_output_mode) is
           when C_OUT_PWM =>
@@ -293,7 +382,11 @@ begin
   -- Process Name     : DATA_FLOW_CTRL
   -- Sensitivity List : I_CLK_125_MHZ : System clock
   --                    s_reset_n     : System reset (active low)
-  -- Useful Outputs   : -- CDL=> Here
+  -- Useful Outputs   : s_pwm_enable  : Enable signal for PWM module
+  --                  : s_clk_enable  : Enable signal for CLK module
+  --                  : s_dac_enable  : Enable signal for DAC module
+  --                  : s_led_0       : LED0 output
+  --                  : s_led_1       : LED1 output
   -- Description      : A process to control the input and output data of the
   --                    system.
   ------------------------------------------------------------------------------
@@ -305,7 +398,6 @@ begin
       s_dac_enable   <= '0';
       s_led_0        <= C_LED_OFF;
       s_led_1        <= C_LED_OFF;
-
 
     elsif (rising_edge(I_CLK_125_MHZ)) then
 
